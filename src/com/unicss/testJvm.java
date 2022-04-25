2d827c64c892bb1a733c908f7437dca684b7034b
dgty2 <forrest2@qq.com>
2022-04-25 18:16:01 +0800

    11

--- src/com/unicss/testJvm.java
+++ src/com/unicss/testJvm.java
@@ -8,6 +8,7 @@ public class testJvm {
         int one = 1;
         int two = 2;
         int thre = 3;
+        int four = 4;
         int result = one + thre + two;
 
         System.out.println("result = " + result);
